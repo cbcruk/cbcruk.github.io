54bbe172b683b45efdd5e70cd9896f0e754ce6f8
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-10-09 09:42:17 +0900

    feat: 태그 검색기능 추가

--- pages/tagged/index.js
+++ pages/tagged/index.js
@@ -1,19 +1,53 @@
 import { getAllTags } from '$lib/airtable'
 import Link from 'next/link'
+import Fuse from 'fuse.js'
 import Layout from '../../components/Layout'
+import { useRef, useState } from 'react'
+import clsx from 'clsx'
 
 /**
  * @param {object} props
  * @param {import('$lib/types').TagRecord[]} props.data
  */
 function Tagged({ data }) {
+  const fuse = useRef(
+    new Fuse(data, {
+      keys: ['fields.select'],
+      minMatchCharLength: 3,
+    })
+  ).current
+  const [refIndexes, setRefIndexes] = useState([])
+
   return (
     <Layout title="Tagged" isShowTitle={false}>
-      <div className="flex flex-wrap gap-2">
-        {data.map((tag) => {
+      <label className="flex flex-col gap-1">
+        <span>🔦</span>
+        <input
+          type="search"
+          className="p-2 px-3 rounded-xl bg-[color:var(--solarized-background-highlight)] text-[color:var(--solarized-green)] text-xs"
+          onChange={(e) => {
+            setRefIndexes(
+              fuse.search(e.target.value).map((item) => item.refIndex)
+            )
+          }}
+        />
+      </label>
+
+      <div className="flex flex-wrap gap-0.5 mt-4">
+        {data.map((tag, index) => {
           return (
             <Link key={tag.id} href={`/memo/search?tags=${tag.fields.select}`}>
-              <a className="p-2 px-3 border border-slate-700 hover:border-transparent rounded-lg mb-[8px] text-xs hover:font-bold hover:bg-[color:var(--solarized-background-highlight)] hover:text-[color:var(--solarized-violet)] ease-out duration-300">
+              <a
+                className={clsx(
+                  'p-1 px-2 rounded-xl mb-[8px] hover:bg-[color:var(--solarized-background-highlight)] text-xs hover:text-[color:var(--solarized-violet)] ease-out duration-300',
+                  {
+                    'text-[color:var(--solarized-violet)]':
+                      refIndexes.includes(index),
+                    'bg-[color:var(--solarized-background-highlight)]':
+                      refIndexes.includes(index),
+                  }
+                )}
+              >
                 #{tag.fields.select}
               </a>
             </Link>
