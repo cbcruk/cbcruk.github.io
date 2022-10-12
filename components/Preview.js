26e990c344d83171073211e344ff43b7e4ea1f3a
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-10-13 08:08:33 +0900

    feat: 메모 아이템 페이지 추가

--- components/Preview.js
+++ components/Preview.js
@@ -1,8 +1,5 @@
 // @ts-check
-import { MemoBody } from './Preview/MemoBody'
-import { MemoDate } from './Preview/MemoDate'
-import { MemoEmbedUrl } from './Preview/MemoEmbedUrl'
-import { MemoTags } from './Preview/MemoTags'
+import { MemoItem } from './Preview/MemoItem'
 
 /**
  *
@@ -13,31 +10,16 @@ import { MemoTags } from './Preview/MemoTags'
 function Preview({ items }) {
   return (
     <div className="Preview items">
-      <style jsx>{`
-        .Preview-item :global(.MemoItem-date) {
-          opacity: 0;
-          transition: 0.3s all ease-out;
-        }
-      `}</style>
       {items.map(({ id, fields }) => {
         return (
-          <div
+          <MemoItem
             key={id}
-            className="Preview-item border-2 border-[color:var(--solarized-background-highlight)] rounded-xl overflow-hidden mt-4 first:mt-0"
-          >
-            <MemoBody serialize={fields.serialize} />
-            <div className="flex justify-between items-center p-4 text-[10px]">
-              <MemoTags tags={fields.tags} />
-              {fields.serialize.frontmatter.embed && (
-                <MemoEmbedUrl url={fields.serialize.frontmatter.embed} />
-              )}
-            </div>
-            <MemoDate
-              className="MemoItem-date"
-              createdAt={fields.createdAt}
-              lastModified={fields.lastModified}
-            />
-          </div>
+            id={id}
+            serialize={fields.serialize}
+            tags={fields.tags}
+            createdAt={fields.createdAt}
+            lastModified={fields.lastModified}
+          />
         )
       })}
     </div>
---
+++ pages/memo/item/[id].js
@@ -0,0 +1,58 @@
+import { serializeHandler } from '$lib/mdx'
+import { getItem, getList, releaseFormula } from '@cbcruk/next-utils'
+import Layout from 'components/Layout'
+import { MemoItem } from 'components/Preview/MemoItem'
+
+function MemoItemPage({ data }) {
+  return (
+    <Layout title="Memo" isShowTitle={false}>
+      <MemoItem
+        serialize={data.fields.serialize}
+        tags={data.fields.tags}
+        createdAt={data.fields.createdAt}
+        lastModified={data.fields.lastModified}
+        className="border-0"
+      />
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const data = await getItem({ url: '/memo', id: params.id })
+
+  data.fields.serialize = await serializeHandler({
+    source: data.fields.body,
+    id: data.id,
+  })
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60,
+  }
+}
+
+export async function getStaticPaths() {
+  const data = await getList({
+    url: '/memo',
+    params: {
+      filterByFormula: releaseFormula(),
+      sort: [{ field: 'lastModified', direction: 'desc' }],
+      fields: ['index'],
+      pageSize: 100,
+    },
+  })
+  const paths = data.records.map((item) => {
+    return {
+      params: { id: item.id },
+    }
+  })
+
+  return {
+    fallback: 'blocking',
+    paths,
+  }
+}
+
+export default MemoItemPage
