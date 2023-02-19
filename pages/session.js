dff28d3ad27b9052e9574f50bd93f5a3bcf9a0ed
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-02-19 09:37:04 +0900

    update: session UI 코드 변경

--- pages/session.js
+++ pages/session.js
@@ -1,38 +1,35 @@
+import { Button } from 'components/Form/Button'
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { match, P } from 'ts-pattern'
+
+function SessionDesc({ children }) {
+  return <p>{children}</p>
+}
 
 function Session() {
   const { data: session } = useSession()
 
   return (
     <div className="p-4 text-xs">
-      {(() => {
-        if (session) {
+      {match(session)
+        .with({ user: { email: P.not(P.nullish) } }, () => {
           return (
-            <>
+            <SessionDesc>
               Signed in as {session.user.email} <br />
-              <button onClick={() => signOut()} className={styles.button}>
-                Sign out
-              </button>
-            </>
+              <Button onClick={() => signOut()}>Sign out</Button>
+            </SessionDesc>
           )
-        }
-
-        return (
-          <>
-            Not signed in <br />
-            <button onClick={() => signIn()} className={styles.button}>
-              Sign in
-            </button>
-          </>
-        )
-      })()}
+        })
+        .otherwise(() => {
+          return (
+            <SessionDesc>
+              Not signed in <br />
+              <Button onClick={() => signIn()}>Sign in</Button>
+            </SessionDesc>
+          )
+        })}
     </div>
   )
-}
-
-const styles = {
-  button:
-    'font-bold bg-[color:var(--solarized-background-highlight)] p-2 rounded-lg mt-2 text-[color:var(--solarized-violet)]',
 }
 
 export default Session
