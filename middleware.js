502cd845de55f71204c4be93977174b5515ad9df
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-02-19 22:27:19 +0900

    update: withAuth 체크 추가

---
+++ middleware.js
@@ -0,0 +1,9 @@
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => token?.role === 'admin',
+  },
+})
+
+export const config = { matcher: ['/admin/:path*'] }
---
+++ pages/admin/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+export function Layout({ children }) {
+  return <div className="p-4 text-xs">{children}</div>
+}
+
+function Admin() {
+  return (
+    <Layout>
+      <nav className="flex gap-2">
+        <Link href="/admin/revalidate">revalidate</Link>
+        <Link href="/admin/links">links</Link>
+      </nav>
+    </Layout>
+  )
+}
+
+export default Admin
