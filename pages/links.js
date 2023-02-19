4c9ece63b581c7999661e7395eab0e22edc8d7aa
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-02-19 10:36:04 +0900

    update: 링크생성 폼 ui 변경

--- pages/links.js
+++ pages/links.js
@@ -1,52 +1,71 @@
 // @ts-check
-import { useCallback, useState } from 'react'
-import Layout from '../components/Layout'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { Button } from 'components/Form/Button'
+import { Textarea } from 'components/Form/Textarea'
+import { toastError } from 'components/Toaster/handler'
+import { useRef } from 'react'
+import { match } from 'ts-pattern'
 
 function Links() {
-  const [result, setResult] = useState('')
-  const handleSubmit = useCallback(async (e) => {
-    e.preventDefault()
+  const formRef = useRef(null)
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const formData = new FormData(formRef.current)
+      const data = formData.get('input')
 
-    const value = e.target.input.value
+      return axios
+        .post('/api/md', {
+          data,
+        })
+        .then((r) => r.data)
+    },
+    onSuccess: async (data) => {
+      await navigator.clipboard.writeText(data)
 
-    if (!value) {
-      return
-    }
-
-    const response = await fetch('/api/md', {
-      method: 'POST',
-      body: e.target.input.value,
-    })
-    const { data } = await response.json()
-
-    setResult(data)
-  }, [])
+      formRef.current.reset()
+    },
+  })
 
   return (
-    <Layout title="링크">
-      <form onSubmit={handleSubmit} className="text-sm mt-4">
-        <textarea
-          name="input"
-          className="block w-full h-40 bg-gray-400 rounded-md p-2 text-black"
-        />
+    <div className="max-w-[400px] p-4 text-xs">
+      <div className="p-2">
+        {match(mutation)
+          .with({ isIdle: true }, () => <p>...</p>)
+          .with({ isLoading: true }, () => (
+            <p>로드 중... 요청을 처리하는 동안 잠시 기다려 주십시오.</p>
+          ))
+          .with({ isError: true }, () => <p>에러가 발생했습니다.</p>)
+          .with({ isSuccess: true }, ({ data }) => (
+            <p title={data}>응답 결과값이 클립보드에 저장되었습니다.</p>
+          ))
+          .otherwise(() => null)}
+      </div>
+
+      <form
+        ref={formRef}
+        onSubmit={(e) => {
+          e.preventDefault()
+
+          const hasInvalid = e.currentTarget.input.value
+            .split('\n')
+            .some((value) => !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))
+
+          if (hasInvalid) {
+            toastError('이메일 형식을 입력해주세요')
+            return
+          }
+
+          mutation.mutate()
+        }}
+        className="mt-2"
+      >
+        <Textarea name="input" required />
         <div className="flex justify-end">
-          <button type="submit" className="bg-blue-600 p-1 rounded-md mt-2">
-            전송
-          </button>
+          <Button type="submit">전송</Button>
         </div>
       </form>
-
-      <hr className="mt-4 mb-4" />
-
-      <pre
-        className="overflow-hidden p-2 bg-green-600 rounded-md text-xs text-black cursor-pointer"
-        onClick={async () => {
-          await navigator.clipboard.writeText(result)
-        }}
-      >
-        {result}
-      </pre>
-    </Layout>
+    </div>
   )
 }
 
