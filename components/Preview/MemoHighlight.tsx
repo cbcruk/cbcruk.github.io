55db0afd2a478189bcf4968f81e48ea53d8ac968
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-04-30 19:24:10 +0900

    fix: 코드블럭 margin값 수정

--- components/Preview/MemoHighlight.tsx
+++ components/Preview/MemoHighlight.tsx
@@ -5,12 +5,18 @@ type Props = JSX.IntrinsicElements['div']
 export function MemoHighlight({ children }: Props) {
   return (
     <div
+      className="MemoHighlight"
       ref={(element) => {
         element?.querySelectorAll('pre code').forEach((code) => {
           highlightElement(code as HTMLElement)
         })
       }}
     >
+      <style jsx>{`
+        .MemoHighlight :global(pre) {
+          margin: 0;
+        }
+      `}</style>
       {children}
     </div>
   )
