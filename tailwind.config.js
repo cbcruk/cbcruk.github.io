c8944b35119b45d79309bdb1d173763107935b45
Eunsoo Lee <cbcruk@users.noreply.github.com>
2023-04-30 17:00:40 +0900

    fix: tailwind 경고 메시지 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'media',
   theme: {
     extend: {
@@ -55,29 +59,6 @@ module.exports = {
         }
       },
     },
-    colors: ({ colors }) => {
-      return {
-        ...colors,
-        solarized: {
-          base00: '#657b83',
-          base01: '#586e75',
-          base02: '#073642',
-          base03: '#002b36',
-          base0: '#839496',
-          base1: '#93a1a1',
-          base2: '#eee8d5',
-          base3: '#fdf6e3',
-          yellow: '#b58900',
-          orange: '#cb4b16',
-          red: '#dc322f',
-          magent: '#d33682',
-          violet: '#6c71c4',
-          blue: '#268bd2',
-          cyan: '#2aa198',
-          green: '#859900',
-        },
-      }
-    },
   },
   variants: {
     extend: {
@@ -85,5 +66,4 @@ module.exports = {
     },
   },
   plugins: [require('@tailwindcss/typography')],
-  mode: 'jit',
 }
