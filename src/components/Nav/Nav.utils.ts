21810373522fe3db311b475e650393a5286923eb
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-03-29 06:59:02 +0900

    fix: 미사용 변수 제거, tab 처리된 부분 수정

--- src/components/Nav/Nav.utils.ts
+++ src/components/Nav/Nav.utils.ts
@@ -1,9 +1,9 @@
-import { match, type MatchFunction } from "path-to-regexp"
+import { match, type MatchFunction } from 'path-to-regexp'
 
 type Pathname = Parameters<MatchFunction<object>>[0]
 
 export function isMatched(pathname: Pathname) {
   return (paths: Array<Pathname>) => {
-    return paths.map(path => match(path)).some(fn => fn(pathname))
+    return paths.map((path) => match(path)).some((fn) => fn(pathname))
   }
 }
