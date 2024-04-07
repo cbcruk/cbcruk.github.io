22fab98ef376f08c4270f321e870f8bdebf4ee27
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-04-07 22:56:59 +0900

    update: gh-pages 환경으로 이동

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,10 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 import partytown from '@astrojs/partytown'
-import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eunsoolee.vercel.app',
+  site: 'https://https://cbcruk.githu.io/',
   integrations: [mdx(), sitemap(), tailwind(), react(), partytown()],
   markdown: {
     shikiConfig: {
@@ -19,10 +18,4 @@ export default defineConfig({
     '/memos': '/memos/1',
     '/memo': '/memos/1',
   },
-  output: 'server',
-  adapter: vercel({
-    isr: {
-      expiration: 60 * 60 * 24,
-    },
-  }),
 })
