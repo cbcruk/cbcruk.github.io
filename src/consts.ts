44672e21524f1d07fc310d8afc484ebd9caf4866
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-04-25 23:39:27 +0900

    test: GA_TRACKING_ID 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = '은수리의 기묘한 모험'
 export const SITE_DESCRIPTION = '은수리의 기묘한 메모 입니다.'
-export const GA_TRACKING_ID = 'G-V8TBYKVNRE'
+export const GA_TRACKING_ID = 'G-SKPL7J3ZF7'
