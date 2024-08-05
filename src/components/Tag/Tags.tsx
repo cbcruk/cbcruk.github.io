4af807d0d2a1951b1598141143f60239f84527a3
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-08-05 20:14:22 +0900

    fix: Tags 컴포넌트 ssr 적용

--- src/components/Tag/Tags.tsx
+++ src/components/Tag/Tags.tsx
@@ -1,18 +1,11 @@
-import { useSearchParamsQuery } from '@components/Search/hooks/useSearchParamsQuery'
 import type { Props } from './Tags.types'
 import clsx from 'clsx'
 import type { CSSProperties } from 'react'
 import { getRandomColor } from './Tags.utils'
 
 export function Tags({ tags }: Props) {
-  const q = useSearchParamsQuery()
-
-  if (q) {
-    return null
-  }
-
   return (
-    <div className="flex flex-wrap gap-2 mt-4 text-xs">
+    <div className="flex flex-wrap gap-2 text-xs">
       {tags.map((tag) => (
         <a
           key={tag}
