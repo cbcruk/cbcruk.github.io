f626a6c1eb786198f28b046231d9c1d1372239e6
Eunsoo Lee <cbcruk@users.noreply.github.com>
2024-08-10 22:13:04 +0900

    fix: `locale` 옵션 추가

--- src/components/Memo/MemoDate.utils.ts
+++ src/components/Memo/MemoDate.utils.ts
@@ -2,5 +2,5 @@ import type { Props } from '@components/Memo/MemoDate.types'
 import { format } from '@formkit/tempo'
 
 export function formatDate(date: Props['ctime' | 'mtime']) {
-  return format(new Date(date), 'full')
+  return format(new Date(date), 'full', 'ko')
 }
