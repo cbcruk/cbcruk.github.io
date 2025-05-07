d8c3184e122da2b48001009491142109f4332df0
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-05-07 23:24:34 +0900

    update: fonts API 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,4 +18,27 @@ export default defineConfig({
     '/memo': '/memos/1',
   },
   output: import.meta.env.DEV ? 'server' : 'static',
+  experimental: {
+    fonts: [
+      {
+        provider: 'local',
+        name: 'Noto Sans KR',
+        cssVariable: '--font-noto-sans-kr',
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/noto-sans-kr-v27-korean_latin-regular.woff2',
+            ],
+          },
+          {
+            weight: 700,
+            style: 'normal',
+            src: ['./src/assets/fonts/noto-sans-kr-v27-korean_latin-700.woff2'],
+          },
+        ],
+      },
+    ],
+  },
 })
