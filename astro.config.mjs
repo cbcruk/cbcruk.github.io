f6af663827de802345e23644973a9d45ac780e2b
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-06-24 11:26:18 +0900

    test: vercel 환경에서 env 확인하기

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
+console.log(import.meta.env)
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cbcruk.github.io',
