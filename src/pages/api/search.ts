e2e32c78426e8345caa13487d89cd1bcc2cfe413
Eunsoo Lee <cbcruk@users.noreply.github.com>
2026-01-01 22:23:31 +0900

    feat: fuse 추가

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,12 +1,12 @@
 export const prerender = process.env.VERCEL ? false : true
 
+import Fuse from 'fuse.js'
 import { getReleaseMemoCollection } from '@collection/memo'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url)
   const q = url.searchParams.get('q')
-  const memoCollection = await getReleaseMemoCollection()
 
   if (!q) {
     return new Response(null, {
@@ -14,7 +14,24 @@ export const GET: APIRoute = async ({ request }) => {
     })
   }
 
-  const result = memoCollection.filter((memo) => memo.body?.includes(q))
+  const memoCollection = await getReleaseMemoCollection()
+
+  const fuse = new Fuse(memoCollection, {
+    keys: [
+      { name: 'data.tags', weight: 2 },
+      { name: 'data.title', weight: 1.5 },
+      { name: 'data.description', weight: 1.2 },
+      { name: 'body', weight: 1 },
+    ],
+    threshold: 0.4,
+    ignoreLocation: true,
+    includeScore: true,
+  })
+
+  const result = fuse.search(q).map(({ item, score }) => ({
+    ...item,
+    score,
+  }))
 
   return Response.json(result, { status: 200 })
 }
